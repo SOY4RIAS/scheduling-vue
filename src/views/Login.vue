8f971cb3aabb5e2156d73437009f08ace9710344
Santiago Arias <santiago.arias@wolox.co>
2021-01-28 14:33:53 -0500

    tailwindcss added

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login-container">
+    <form></form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+}
+</style>
